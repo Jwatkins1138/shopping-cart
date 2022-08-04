b0f841740aeae7023eebe589d4dd36ed3d495e54
Jason Watkins <goushoryuken@gmail.com>
2022-08-04 11:04:50 -0700

    init style header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <nav className="head-nav"></nav>
+      <div className="head-title"><h1>shop type shop</h1></div>
+      <nav className="head-nav"></nav>
+    </header>
+  );
+};
+
+export default Header;
